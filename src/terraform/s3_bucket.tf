3b20e0164f3f3bc79f462c7c1804f5a8945a561c
Caio-H3nr1que <121584527+Caio-H3nr1que@users.noreply.github.com>
2025-07-02 17:36:22 -0300

    ajustes

--- src/terraform/s3_bucket.tf
+++ src/terraform/s3_bucket.tf
@@ -1,6 +1,6 @@
 # S3 Bucket
 resource "aws_s3_bucket" "main_bucket" {
-  bucket = "terraform-state-aws-academy-2025"
+  bucket = "terraform-state-aws-academy-2025-caio-henrique-gustavo-joao"
 }
 
 variable "student_id" {
