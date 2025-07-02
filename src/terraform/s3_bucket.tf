a38b51f44434c50e93bb8a1fbea450b38ad6315b
Caio-H3nr1que <121584527+Caio-H3nr1que@users.noreply.github.com>
2025-07-02 17:26:09 -0300

    ajustes nome bucket

--- src/terraform/s3_bucket.tf
+++ src/terraform/s3_bucket.tf
@@ -1,6 +1,6 @@
 # S3 Bucket
 resource "aws_s3_bucket" "main_bucket" {
-  bucket = "${var.project_name}-${var.student_id}-bucket"
+  bucket = "terraform-state-aws-academy-2025-${var.student_id}"
 }
 
 variable "student_id" {
