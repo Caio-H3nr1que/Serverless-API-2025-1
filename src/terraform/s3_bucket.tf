2947a7c7d3a7980c611d71e7e515fee96b5af94d
Caio-H3nr1que <121584527+Caio-H3nr1que@users.noreply.github.com>
2025-07-02 17:32:23 -0300

    Update s3_bucket.tf

--- src/terraform/s3_bucket.tf
+++ src/terraform/s3_bucket.tf
@@ -1,6 +1,6 @@
 # S3 Bucket
 resource "aws_s3_bucket" "main_bucket" {
-  bucket = "terraform-state-aws-academy-2025-${var.student_id}"
+  bucket = "terraform-state-aws-academy-2025"
 }
 
 variable "student_id" {
