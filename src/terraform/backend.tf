81b6f38b8cad8838e169e224806e5e0239ee4f97
Caio-H3nr1que <121584527+Caio-H3nr1que@users.noreply.github.com>
2025-07-02 17:27:46 -0300

    Update backend.tf

--- src/terraform/backend.tf
+++ src/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-aws-academy-2025-${var.student_id}"
+    bucket = "terraform-state-aws-academy-2025-caio-henrique"
     key    = "academy-lab/terraform.tfstate"
     region = "us-east-1"
   }
